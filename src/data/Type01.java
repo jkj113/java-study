e7f4963af04bde12102bbf54c55f6ab15829fa1e
Administrator <Administrator@USER-PC>
2019-02-13 11:35:06 +0900

    첫 자바프로젝트 깃 연동

---
+++ src/data/Type01.java
@@ -0,0 +1,28 @@
+package data;
+
+public class Type01 {
+	
+	public static void main(String[] args) {
+		//문자
+		char c = 'a';
+		
+		//숫자-정수
+		byte b = 127;
+		b = -128;
+		
+		short sh = 123;
+		
+		int i = 123123123;
+		
+		long l = 10000000000l;
+		
+		//숫자 - 실수
+		float f = 1.2f; //기본 실수형은 double
+		double db = 1.2;
+		
+		//논리형(불린)
+		boolean b1 = true;
+		
+	}
+
+}
